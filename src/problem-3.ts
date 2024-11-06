fb7acc8938708f0581d53d534edb77c6ebd2879f
Ridwan-Suhel <ridwansuhel96@gmail.com>
2024-11-07 00:02:33 +0600

    changes added with solving problem-3 by coungting word occurrences

---
+++ src/problem-3.ts
@@ -0,0 +1,19 @@
+{
+
+    // declaring a type for countWordOccurrences where i expect a number in return and two string values as arguement
+    type CountWord = (sentence: string, word: string) => number;
+    
+    // creating a function for coungting word occurrences
+    const countWordOccurrences: CountWord = (sentence, word) => {
+        const lowerCaseSentence: string = sentence.toLowerCase();
+        const lowerCaseWord: string = word.toLowerCase();
+        const sentenceSplit: number = lowerCaseSentence.split(lowerCaseWord).length - 1;
+        return sentenceSplit 
+    }
+
+    // call the function here with a sentence and a word as arguement of countWordOccurrences function
+    // const sentence: string = "TypeScript is great. I love TypeScript!";
+    // const word: string = "typescript";
+    // console.log(countWordOccurrences(sentence, word))
+
+}
